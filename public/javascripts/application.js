cf55bd1b27924a0264c4eb2daf29748030b583d9
Ward W. Vuillemot <wwv@mac.com>
2012-08-29 00:50:12 -0700

    Background
    - randomly select one of 12 zodiacs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 	
-$(window).load( function () {
+function BackgroundFlip() {
+	  image = Math.floor( Math.random() * 11 );
+    $('.constellation-background').css('background-image', 'url(/images/background_' + image + '.jpg)');
+}
 
+$(window).load( function () {
+	BackgroundFlip();
 });
