56a408e68024e6e040629aa450b00030fab9d24a
Ward W. Vuillemot <wwv@mac.com>
2012-08-29 23:25:16 -0700

    CLAF

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 	
-function BackgroundFlip() {
+function BackgroundFlip(css_item) {
 	  image = Math.floor( Math.random() * 11 );
-    $('.constellation-background').css('background-image', 'url(/images/background_' + image + '.jpg)');
+    $(css_item).css('background-image', 'url(/images/background_' + image + '.jpg)');
 }
 
 $(window).load( function () {
-	BackgroundFlip();
+	BackgroundFlip('.constellation-background');
 });
